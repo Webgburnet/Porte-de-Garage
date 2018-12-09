08b6f9ab75e1e44920556f1ff81be56751a30369
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2018-12-09 10:26:05 +0100

    Add files via upload

---
+++ Arduino/IR/IR.ino
@@ -0,0 +1,29 @@
+/*
+ Programme créer par Simon, projet Garage automatisé (Capteur Infrarouge)
+ Version du programme: v1
+ 
+ */
+
+
+
+#include <IRremote.h>  // Bibliothèque utilisé pour le capteur infrarouge.
+
+#define PIN_IR 3  // Emeteur infrarouge en D3.
+#define PIN_DETECT 2  // Récepteur Infrarouge en D2.
+#define PIN_STATUS 7  // LED en D7.
+
+IRrecv irrecv(PIN_IR);  // Déclaration de l'émetteur infrarouge, celui qui va envoyer le signal analogique.
+IRsend irsend;  // Activation du mode receveur pour le capteur receveur infrarouge.         
+
+void setup()
+{
+  pinMode(PIN_DETECT, INPUT);  // Déclaration du récepteur en entrée digital.
+  pinMode(PIN_STATUS, OUTPUT);  // Déclaration de la LED en sortie digital.
+  irsend.enableIROut(38);  // Configuration du signal envoyé sur 38k hertz.
+  irsend.mark(0);
+}
+
+void loop() {
+  digitalWrite(PIN_STATUS, !digitalRead(PIN_DETECT));  // LED = Allumé SI PIN_DETECT = 1
+}
+
